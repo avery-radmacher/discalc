52a49417871ec567325ff7bf14eb356bcf10c1e7
avery.radmacher <avery.radmacher@gmail.com>
2022-09-04 16:56:14 -0400

    Add option types

--- src/options.rs
+++ src/options.rs
@@ -1,3 +1,29 @@
 pub struct Options {
-    // one field for each configurable option
+    pub rounds_to_double_weight_rounding_mode: RoundingMode,
+    pub same_day_round_ordering: SameDayRoundOrdering,
+    pub bad_round_exclusion_average: BadRoundExclusionAverage,
+    pub bad_round_exclusion_standard_deviation: BadRoundExclusionStandardDeviation,
+    pub final_average_rounding_mode: RoundingMode,
+}
+
+pub enum RoundingMode {
+    Floor,
+    Ceiling,
+    MidpointUp,
+    MidpointToEven,
+}
+
+pub enum SameDayRoundOrdering {
+    Chronological,
+    Reversed,
+}
+
+pub enum BadRoundExclusionAverage {
+    Weighted,
+    Even,
+}
+
+pub enum BadRoundExclusionStandardDeviation {
+    Population,
+    Sample,
 }
