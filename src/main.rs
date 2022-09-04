246283ac5ba1646f70581c19374dff6151f06308
avery.radmacher <avery.radmacher@gmail.com>
2022-09-04 16:27:56 -0400

    Add missing crate info

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ mod options;
 mod reader;
 
 fn main() {
-    // reader/csv | read file from program args
-    // reader     | read into internal format
-    // options    | then for each combination of options:
-    // self       |     for each valid date in the format:
-    // self/core  |         calculate rating
-    // self       |         calculate error from projected
-    // self       |     aggregate errors
-    // self       | order combinations by least error
-    // self       | display results
+    // reader & csv   | read file from program args
+    // reader & chrono| read into internal format
+    // options        | then for each combination of options:
+    // self           |     for each valid date in the format:
+    // self/core      |         calculate rating
+    // self           |         calculate error from projected
+    // self           |     aggregate errors
+    // self           | order combinations by least error
+    // self           | display results
 }
