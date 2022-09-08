9df1674dced8911daeca71e27e53e8da5efb56ff
avery.radmacher <avery.radmacher@gmail.com>
2022-09-08 13:30:21 -0400

    Remove warnings and make run

--- src/core.rs
+++ src/core.rs
@@ -9,16 +9,12 @@ pub struct Round {
     pub rating: u32,
 }
 
-pub fn calculate_rating(rounds: Vec<Round>, options: Options, date: Option<NaiveDate>) -> u32 {
-    todo!()
+pub fn calculate_rating(_rounds: Vec<Round>, _options: Options, _date: Option<NaiveDate>) -> u32 {
+    1000 // TODO
 }
 
 pub fn calculate_rating_on(rounds: Vec<Round>, options: Options, date: NaiveDate) -> u32 {
     calculate_rating(rounds, options, Some(date))
-}
-
-pub fn calculate_rating_latest(rounds: Vec<Round>, options: Options) -> u32 {
-    calculate_rating(rounds, options, None)
 }
 
 pub struct OptionsWithError {
