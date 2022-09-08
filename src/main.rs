5bb2e99c6f60d0def23b0268ffc9116275748883
avery.radmacher <avery.radmacher@gmail.com>
2022-09-08 13:29:52 -0400

    Implement main

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-mod core;
-mod options;
-mod reader;
+use crate::core::{calculate_rating_on, OptionsWithError};
+use options::get_all_options;
 
-fn main() -> Result<(), reader::Error> {
-    // reader & csv   | read file from program args
-    // reader & chrono| read into internal format
-    let (rounds, dates_with_ratings) = reader::read(std::io::stdin())?;
-    // options        | then for each combination of options:
-    // self           |     for each valid date in the format:
-    // self/core      |         calculate rating
-    // self           |         calculate error from projected
-    // self           |     aggregate errors
-    // self           | order combinations by least error
-    // self           | display results
+mod core;
+mod io_handler;
+mod options;
+
+fn main() -> Result<(), io_handler::Error> {
+    let (rounds, dates_with_ratings) = io_handler::read(std::io::stdin())?;
+    let mut results = get_all_options()
+        .into_iter()
+        .map(|options| {
+            let unscaled_error: u32 = dates_with_ratings
+                .iter()
+                .map(|(date, rating)| {
+                    let projected_rating = calculate_rating_on(rounds.clone(), options, *date);
+                    (projected_rating - rating).pow(2)
+                })
+                .sum();
+            OptionsWithError {
+                options,
+                error: unscaled_error as f64 / rounds.len() as f64,
+            }
+        })
+        .collect::<Vec<_>>();
+    results.sort_unstable_by(|a, b| a.error.partial_cmp(&b.error).unwrap());
+    io_handler::write(std::io::stdout(), results)?;
     Ok(())
 }
